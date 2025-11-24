8a8c4bfd07a464c64c8577f87aa6de2ea320476c
zen-patilrohit <patil.rohit14@gmail.com>
2025-11-24 11:24:53 +0530

    frontend issues

--- src/pages/Tenants.tsx
+++ src/pages/Tenants.tsx
@@ -46,7 +46,7 @@ import {
   Phone,
   MapPin,
 } from "lucide-react";
-import { tenantsApiService } from "@/services/Leasing_Tenants/tenantsapi";
+import { tenantsApiService } from "@/services/leasing_tenants/tenantsapi";
 import { siteApiService } from "@/services/spaces_sites/sitesapi";
 import { Tenant, TenantOverview } from "@/interfaces/leasing_tenants_interface";
 import { useSkipFirstEffect } from "@/hooks/use-skipfirst-effect";
@@ -230,8 +230,7 @@ const Tenants = () => {
     if (response?.success) {
       setIsFormOpen(false);
       toast.success(
-        `Tenant ${tenantData.name} has been ${
-          formMode === "create" ? "created" : "updated"
+        `Tenant ${tenantData.name} has been ${formMode === "create" ? "created" : "updated"
         } successfully.`
       );
     }
