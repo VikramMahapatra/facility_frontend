ef2af97cf2a0f96ed556bc1f780dbe93ee08d386
zen-patilrohit <patil.rohit14@gmail.com>
2026-02-11 14:03:13 +0530

    super admin changes

--- src/pages/TicketWorkOrders.tsx
+++ src/pages/TicketWorkOrders.tsx
@@ -259,10 +259,8 @@ export default function TicketWorkOrders() {
     if (response?.success) {
       setIsFormOpen(false);
       toast.success(
-        `Ticket work order has been ${
-          formMode === "create" ? "created" : "updated"
-        }
-        } successfully.`
+        `Ticket work order has been ${formMode === "create" ? "created" : "updated"}
+        successfully.`
       );
     }
   };
